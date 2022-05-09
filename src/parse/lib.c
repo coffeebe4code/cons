f1e502367ec9fda0707c5cd9180fa0014b5113e9
Chris <chris.clark@coffeeb4code.com>
2022-05-08 21:34:35 -0500

    have to read up on parser and do bnf first

--- src/parse/lib.c
+++ src/parse/lib.c
@@ -19,48 +19,14 @@ parser_source_t parser_new() {
   return val;
 }
 
-option_t parse_expr(lex_source_t *lexer, parser_source_t *parser) {
-  if (parse_bin(peeked)) {
-    check_size(parser);
-    lexeme_t val = lex_collect(lexer);
-    memcpy(&parser->lexemes[parser->len], &val, sizeof(lexeme_t));
-    ast_t ast = AST_LIT(&parser->lexemes[parser->len]);
-    memcpy(&parser->asts[parser->len], &ast, sizeof(ast_t));
-    return OPTY(&parser->asts[parser->len]);
-  }
-  return OPTN();
-}
-
-option_t parse_bin(lex_source_t *lexer, parser_source_t *parser) {
-  token_e peeked = lex_peek(lexer).tok;
-  if (is_lit(peeked)) {
-    check_size(parser);
-    lexeme_t val = lex_collect(lexer);
-    memcpy(&parser->lexemes[parser->len], &val, sizeof(lexeme_t));
-    ast_t ast = AST_LIT(&parser->lexemes[parser->len]);
-    memcpy(&parser->asts[parser->len], &ast, sizeof(ast_t));
-    return OPTY(&parser->asts[parser->len]);
-  }
-  return OPTN();
-}
-
-option_t parse_lit(lex_source_t *lexer, parser_source_t *parser) {
-  token_e peeked = lex_peek(lexer).tok;
-  if (is_lit(peeked)) {
-    check_size(parser);
-    lexeme_t val = lex_collect(lexer);
-    memcpy(&parser->lexemes[parser->len], &val, sizeof(lexeme_t));
-    ast_t ast = AST_LIT(&parser->lexemes[parser->len]);
-    memcpy(&parser->asts[parser->len], &ast, sizeof(ast_t));
-    return OPTY(&parser->asts[parser->len]);
-  }
-  return OPTN();
-}
-
-int parse_un(lex_source_t *lexer) {
-  token_e peeked = lex_peek(lexer).tok;
-  if (is_un_op(peeked)) {
-    return 1;
-  }
-  return 0;
-}
+// option_t parse_expr(lex_source_t *lexer, parser_source_t *parser) {
+//  return OPTN();
+//}
+//
+// option_t parse_bin(lex_source_t *lexer, parser_source_t *parser) {
+//  return OPTN();
+//}
+//
+// option_t parse_lit(lex_source_t *lexer, parser_source_t *parser) {
+//  return OPTN();
+//}
