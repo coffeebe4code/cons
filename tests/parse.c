b687bf8828fa239c2ef65645d29f86212acfadde
Chris <chris.clark@coffeeb4code.com>
2022-05-16 07:49:48 -0500

    low bin parses

--- tests/parse.c
+++ tests/parse.c
@@ -42,27 +42,39 @@ void test_terminal() {
 }
 
 void test_bin_op() {
-  char *input = "2 * 2";
+  char *input = "1 + 2 * 2";
   lex_source_t lex = lex_new(input);
   parser_source_t parse = parser_new();
+  MOCK(lex_peek, (lexeme_t){.tok = Num});
+  MOCK(lex_peek, (lexeme_t){.tok = Plus});
   MOCK(lex_peek, (lexeme_t){.tok = Num});
   MOCK(lex_peek, (lexeme_t){.tok = Mul});
   MOCK(lex_peek, (lexeme_t){.tok = Num});
   lexeme_t mocked_lex = (lexeme_t){.tok = Num, .span = (span_t){.ptr = input}};
   MOCK(lex_collect, mocked_lex);
-  mocked_lex = (lexeme_t){.tok = Mul, .span = (span_t){.ptr = input + 2}};
+  mocked_lex = (lexeme_t){.tok = Plus, .span = (span_t){.ptr = input + 2}};
   MOCK(lex_collect, mocked_lex);
   mocked_lex = (lexeme_t){.tok = Num, .span = (span_t){.ptr = input + 4}};
   MOCK(lex_collect, mocked_lex);
+  mocked_lex = (lexeme_t){.tok = Mul, .span = (span_t){.ptr = input + 6}};
+  MOCK(lex_collect, mocked_lex);
+  mocked_lex = (lexeme_t){.tok = Num, .span = (span_t){.ptr = input + 8}};
+  MOCK(lex_collect, mocked_lex);
+  MOCK(is_num, 1);
+  MOCK(is_low_bin, 1);
   MOCK(is_num, 1);
   MOCK(is_high_bin, 1);
   MOCK(is_num, 1);
 
-  ast_t *val = parse_high_bin(&lex, &parse);
+  ast_t *val = parse_low_bin(&lex, &parse);
   ASSERT(val->expr_kind == BinOp);
-  ASSERT((int)val->tok1.bin_left_expr->tok1.number.raw == 2);
-  ASSERT(val->tok2.bin_op == Mul);
-  ASSERT((int)val->tok3.bin_right_expr->tok1.number.raw == 2);
+  ASSERT((int)val->tok1.bin_left_expr->tok1.number.raw == 1);
+  ASSERT(val->tok2.bin_op == Plus);
+  ASSERT((int)val->tok3.bin_right_expr->tok1.bin_left_expr->tok1.number.raw ==
+         2);
+  ASSERT(val->tok3.bin_right_expr->tok2.bin_op == Mul);
+  ASSERT((int)val->tok3.bin_right_expr->tok3.bin_right_expr->tok1.number.raw ==
+         2);
 
   parser_free(&parse);
 }
