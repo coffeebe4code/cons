afef8b7a88c1dc02d3e65abb768ad012eb33762a
Chris <chris.clark@coffeeb4code.com>
2022-05-30 07:49:05 -0500

    going with an intermediate representation model

--- src/config/lib.c
+++ src/config/lib.c
@@ -1,6 +1,6 @@
-#include "../../include/uni.h"
+#include "../../include/config.h"
 
 void __attribute__((constructor)) set_uni_data() {
   int i = 1;
-  uni_vals.is_little_endian = (*((char *)&(i)));
+  config_vals.is_little_endian = (*((char *)&(i)));
 }
--- tests/config.c
+++ tests/config.c
@@ -1,10 +1,10 @@
 #define MORE_COFFEE
 #define WITH_MOCKING
 #include "../barista.h"
-#include "../include/uni.h"
+#include "../include/config.h"
 
 int main() {
-  DESCRIBE("uni");
+  DESCRIBE("config");
   SHOULDB("work", { ASSERT(1 == 1); });
   RETURN();
 }
