82defdd2fd2a87673c29844dcb048315cb48215c
Chris Clark <chris.clark@coffeeb4code.com>
2022-06-20 13:38:06 -0500

    size_t need to implement hashes for labels/symbols

--- include/ir.h
+++ include/ir.h
@@ -10,6 +10,7 @@
 LIST_DECL(block_t, blocks);
 typedef struct {
   blocks_l blocks;
+  block_t curr_block;
   gen_source_t gen;
   size_t new_idx;
   size_t main_exit;
@@ -22,7 +23,7 @@ void ir_clean(ir_source_t *source);
 void ir_free(ir_source_t *source);
 
 size_t ir_constf64(ir_source_t *source, byte8_t left);
-size_t ir_addf64(ir_source_t *source, byte8_t left, byte8_t right);
+size_t ir_addf64(ir_source_t *source, size_t left, size_t right);
 void ir_mulf64(ir_source_t *source, byte8_t left, byte8_t right);
 void ir_div64(ir_source_t *source, byte8_t left, byte8_t right);
 void ir_sub64(ir_source_t *source, size_t left, size_t right);
