7cf86fc22ff9ecc03e2e4fa051a829b3219a220e
Christopher Clark <chris.clark@coffeeb4code.com>
2022-07-19 21:31:07 -0500

    works on my local cygwin

--- include/error.h
+++ include/error.h
@@ -19,11 +19,12 @@ typedef struct {
     char *message;
   } first;
   int line_no;
-} error_t;
+} cons_error_t;
 
 #define ERR_FORMAT(STRING_INDEX, FIRST_TO_CHECK)                               \
   __attribute__((format(printf, STRING_INDEX, FIRST_TO_CHECK)))
 
 void BUILDER(FILE *stream, char *tag, char *kind, char *fmt, va_list args);
-void ERROR(error_t *err, char *fmt, ...);
-void ERROR_PTR(error_t *err, char *message, int pointer_loc, char *fmt, ...);
+void ERROR(cons_error_t *err, char *fmt, ...);
+void ERROR_PTR(cons_error_t *err, char *message, int pointer_loc, char *fmt,
+               ...);
